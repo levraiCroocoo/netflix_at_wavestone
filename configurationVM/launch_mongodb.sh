b4a742f5f2c023033aa10f48701d0552c78902db
levraiCroocoo <146856137+levraiCroocoo@users.noreply.github.com>
2025-01-21 09:50:31 +0100

    Update launch_mongodb.sh

--- configurationVM/launch_mongodb.sh
+++ configurationVM/launch_mongodb.sh
@@ -1,16 +1,20 @@
 
 
-# Création du fichier de dépôt MongoDB
-echo "[mongodb-org-8.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" | sudo tee /etc/yum.repos.d/mongodb-org-8.0.repo
 
-# Install MongoDB packages
+
+#!/bin/bash
+
+mongodb_directory="/etc/yum.repos.d/"
+# Check that the mongodb repo exists
+mongodb_repo=$mongodb_directory/mongodb-org-8.0.repo
+if [[ ! -f $"mongodb_repo" ]]; then
+        sudo echo "[mongodb-org-8.0]
+        name=MongoDB Repository
+        baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
+        gpgcheck=1
+        enabled=1
+        ggpgkey=https://pgp.mongodb.com/server-8.0.asc" > mongodb_repo
+fi
+
 sudo yum install -y mongodb-org
-
-# Start MongoDB service
 sudo systemctl start mongod
-sudo systemctl enable mongod
